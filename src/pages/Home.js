3ee2799dc9ea3ca4243848449235bd377f7915cf
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2022-02-15 12:05:19 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
+import { Carousel, Card } from "antd";
 import Header from "../components/Header";
+import Carousel1 from "../carousel1.png";
+import Carousel2 from "../carousel2.png";
+import Carousel3 from "../carousel3.png";
 import "./Home.css";
+
+const carouselImages = [Carousel1, Carousel2, Carousel3];
 
 const Home = () => (
   <div className="container">
-    <Header/>
-     <Link to="/product" className="link">Product Page</Link>
-     <Link to="/categories" className="link">Categories Page</Link>
-     <Link to="/checkout" className="link">Checkout Page</Link>
+    <Header />
+
+    <Carousel autoplay className="carousel">
+      {carouselImages.map((e) => {
+        return (
+          <img src={e} className="carousel-img" alt="carousel image"></img>
+        );
+      })}
+    </Carousel>
+    <div className="cards">
+      <Card className="card">
+        <h1>Shop By Category</h1>
+      </Card>
+      <Card className="card">
+        <h1>Browse Electronics</h1>
+      </Card>
+      <Card className="card">
+        <h1>Artemis Fowl</h1>
+      </Card>
+      <Card className="card">
+        <h1>Marvel Heroes Ep. 21</h1>
+      </Card>
+    </div>
+
+    <Link to="/product" className="link">
+      Product Page
+    </Link>
+    <Link to="/categories" className="link">
+      Categories Page
+    </Link>
+    <Link to="/checkout" className="link">
+      Checkout Page
+    </Link>
   </div>
 );
 
